e6c995ed4e128159757659b715f8ea427a5f68e4
NIKITA <niki6ka@gmail.com>
2023-02-15 10:50:30 +0300

    working programm

---
+++ 1/Program2-1.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Please enter a three-digit number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number / 100 < 1 || Number / 100 > 10)
+{ Console.WriteLine("It's not a three digit number!!! Are you a stupid?"); }
+else
+{
+    int a = Number % 100;
+    int b = a / 10;
+    Console.WriteLine("The second digit of this " + Number + " three-digit number is " + b);
+}
